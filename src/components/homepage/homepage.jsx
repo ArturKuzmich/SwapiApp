9bdd5789486839607c9d8328c39e4bd4f9ab67ec
Artur <Artur1Kuzmich@gmail.com>
2020-12-17 00:21:29 +0200

    added animation in home page

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './style.css'
-import TopBar from "../top_bar/top_bar";
 
 const HomePage = () => {
     return (
-        <div className='home_page'>
-            asdasd
-        </div>
+            <div className="starwars-demo">
+                <img src="//cssanimation.rocks/demo/starwars/images/star.svg" alt="Star" className="star" />
+                    <img src="//cssanimation.rocks/demo/starwars/images/wars.svg" alt="Wars" className="wars" />
+                        <h2 className="byline" id="byline">The Force Awakens</h2>
+            </div>
     )
 }
 
