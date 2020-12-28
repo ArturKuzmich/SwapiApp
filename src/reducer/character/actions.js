67e4ad8cc31d2b67c26d1904292dd2d42d6dbfbc
Artur <Artur1Kuzmich@gmail.com>
2020-12-28 20:02:48 +0200

    create action for get character films list > next step films reducer

--- src/reducer/character/actions.js
+++ src/reducer/character/actions.js
@@ -1,16 +1,17 @@
 import {API_URL} from "../../constants";
 export const SET_CHOOSED_CHARACTER = 'SET_CHOOSED_CHARACTER';
-export const SET_CHOOSED_PROFILE = 'SET_CHOOSED_PROFILE'
-export const SET_CHARACTER_WORLD = 'SET_CHARACTER_WORLD'
+export const SET_CHOOSED_PROFILE = 'SET_CHOOSED_PROFILE';
+export const SET_CHARACTER_WORLD = 'SET_CHARACTER_WORLD';
+export const SET_CHARACTER_FILMS = 'SET_CHARACTER_FILMS';
 
 export function getChoosedProfile(id) {
     return dispatch => {
         fetch(`${API_URL}/people/${id}`)
             .then(res => res.json())
             .then(profile => {
-                console.log('profile',profile)
                 dispatch(setChoosedProfile(profile))
                 dispatch(getCharacterWorld(profile.homeworld));
+                dispatch(getCharacterFilms(profile.films))
             })
     }
 }
@@ -21,6 +22,23 @@ export function getCharacterWorld(url){
             .then(world =>
                 dispatch(setCharacterWorld(world))
             )
+}
+export function getCharacterFilms(filmsUrl){
+    return dispatch =>
+        Promise.all(filmsUrl.map(filmUrl =>
+            fetch(filmUrl)
+                .then(res => res.json())
+        ))
+            .then(films =>
+                dispatch(setCharacterFilms(films))
+            )
+}
+
+export function setCharacterFilms(films){
+    return {
+        type: SET_CHARACTER_FILMS,
+        films
+    }
 }
 export function setCharacterWorld(world){
     return{
@@ -34,7 +52,6 @@ export function setChoosedProfile(profile) {
         profile
     }
 }
-
 export function setChoosedCharacter(id) {
     return {
         type: SET_CHOOSED_CHARACTER,
--- src/reducer/character/index.js
+++ src/reducer/character/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import id from './id'
 import profile from './profile'
 import world from './world'
+import films from './films'
 
 export default combineReducers({
     id,
     profile,
-    world
+    world,
+    films
 });
